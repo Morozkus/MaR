12a4c2aca16a3ee3c00f7dc042a0106c1fb8ee3f
student <student@spbacu.ru>
2023-12-13 13:37:01 +0300

    Updates

--- src/utils/random.ts
+++
@@ -1,4 +0,0 @@
-export const getDobbleRandomNumber = (row: number, column: number): [number, number] =>
-  [randomInteger(0, row - 1), randomInteger(0, column - 1)]
-
-const randomInteger = (min: number, max: number) => Math.floor(min + Math.random() * (max + 1 - min))
--- src/utils/storage.ts
+++
@@ -1,2 +0,0 @@
-export const getFromStorage = (key: string): string | null => localStorage.getItem(key)
-export const setForStorage = <T>(key: string, value: T | any): void => localStorage.setItem(key, String(value))
